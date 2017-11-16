e753d01118074661a393ff5ec556f9d223aa7c7b
Alex Kucher <a.kucher@avantize.com>
2017-11-16 13:12:02 +0200

    Added MSVC2017 build scripts.

---
+++ scripts/cmakegen-vc15-x64-release.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+:: Check build directory.
+set BUILD_DIR=..\build\vc15-x64-release
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+pushd %BUILD_DIR%
+
+:: Run CMake.
+echo [*] CMake is running...
+cmake ^
+	-G"Visual Studio 15 2017 Win64" ^
+	"-DCMAKE_CONFIGURATION_TYPES:STRING=Release" ^
+	"-DQT_ROOT:PATH=%QT_ROOT%\msvc2017_64" ^
+	../..
+if errorlevel 1 goto :error
+
+:: Exit
+echo [*] Project generation succeeded!
+popd
+goto :eof
+
+:: Error handling
+:error
+echo [!] ERROR: Failed to generate project - see above and correct.
+popd
+exit /b 1
